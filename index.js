aad6b3c78dbb587bca6e6537f086882491062deb
Indrapreet Singh <indrapreet.singh@enquero.com>
2021-02-28 11:35:48 +0530

    Cosmetic changes.

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const ipUtility = document.querySelector(".ip-utility");
 const unixTimestamp = document.querySelector(".unix-timestamp");
 const epochTimestamp = document.querySelector("#epoch-timestamp");
 const localTime = document.querySelector("#local-time");
+const epochBtn = document.querySelector("#epoch-btn");
+const epochOutput = document.querySelector("#epoch-output");
 
 //Epoch converter Events
-localTime.addEventListener("click", ()=>convertToLocal);
+epochBtn.addEventListener("click", () => convertToLocal());
 
+const convertToLocal = () => {
+  epochOutput.innerHTML = new Date(Math.round(parseInt(epochTimestamp.value)/1000));
+};
 
-fetch('https://api.ipify.org/?format=json')
-  .then(response => response.json())
-  .then(data => ipUtility.innerHTML = JSON.stringify(data["ip"]).replaceAll('"',""));
+fetch("https://api.ipify.org/?format=json")
+  .then((response) => response.json())
+  .then(
+    (data) =>
+      (ipUtility.innerHTML = JSON.stringify(data["ip"]).replaceAll('"', ""))
+  );
 
-
-  //Events
-  setInterval(function()
-  {
-      unixTimestamp.innerHTML = "CURRENT UNIX TIMESTAMP IS-"+Math.round(Date.now() / 1000);
-  },1000)
-
-  convertToLocal = ()=>{
-      localTime,innerHTML = new Date(0).setUTCSeconds(epochTimestamp.value);
-  }
+//Events
+setInterval(function () {
+  unixTimestamp.innerHTML =
+    "CURRENT UNIX TIMESTAMP IS-" + Math.round(Date.now() / 1000);
+}, 1000);
