a73750867284af699a21f754b5aadbadc9d43eac
Indrapreet Singh <indrapreet.singh@enquero.com>
2021-03-04 08:57:31 +0530

    FIx allignments.

--- index.js
+++ index.js
@@ -14,63 +14,52 @@ const toLocalOutput = document.querySelector("#to-local-output");
 const toEpochOutput = document.querySelector("#to-epoch-output");
 
 //Select a particular utility
-ipFinder.addEventListener("click", ()=>selectUtility("IP_FINDER"));
-epochConverter.addEventListener("click", ()=>selectUtility("EPOCH_UTILITY"));
+ipFinder.addEventListener("click", () => selectUtility("IP_FINDER"));
+epochConverter.addEventListener("click", () => selectUtility("EPOCH_UTILITY"));
 
 //Epoch converter Events
 toLocalBtn.addEventListener("click", () => convertToLocal());
-toEpochBtn.addEventListener("click", ()=>convertToEpoch());
+toEpochBtn.addEventListener("click", () => convertToEpoch());
 
-selectUtility = (utility)=>
-{  
-  switch(utility)
-  {
-    case "IP_FINDER" : 
-          utilityInfo.innerHTML = "YOUR IP ADDRESS IS";
-          ipUtility.style.display = "block";
-          epochUtility.style.display = "none";
-          break;
-    case "EPOCH_UTILITY" :
-          utilityInfo.innerHTML = "EPOCH UTILITY";
-          epochUtility.style.display ="block";
-          ipUtility.style.display = "none"
-    break;
+selectUtility = (utility) => {
+  switch (utility) {
+    case "IP_FINDER":
+      utilityInfo.innerHTML = "YOUR IP ADDRESS IS";
+      ipUtility.style.display = "block";
+      epochUtility.style.display = "none";
+      break;
+    case "EPOCH_UTILITY":
+      utilityInfo.innerHTML = "EPOCH UTILITY";
+      epochUtility.style.display = "block";
+      ipUtility.style.display = "none";
+      break;
   }
-   
-}
+};
 
 const convertToLocal = () => {
-  if(epochTimestamp.value.length===0)
-  {
-    toLocalOutput.innerHTML = "Input can't be empty!"  
-  }
-  else
-  {
-  toLocalOutput.innerHTML = new Date(epochTimestamp.value*1000);
+  if (epochTimestamp.value.length === 0) {
+    toLocalOutput.innerHTML = "Input can't be empty!";
+  } else {
+    toLocalOutput.innerHTML = new Date(epochTimestamp.value * 1000);
   }
 };
 
 const convertToEpoch = () => {
   const y = parseInt(document.querySelector("#year-input").value);
-  const m = parseInt(document.querySelector("#month-input").value)-1;
+  const m = parseInt(document.querySelector("#month-input").value) - 1;
   const d = parseInt(document.querySelector("#day-input").value);
   const hh = parseInt(document.querySelector("#hour-input").value);
   const mm = parseInt(document.querySelector("#min-input").value);
   const ss = parseInt(document.querySelector("#sec-input").value);
 
-  const date = Math.round(new Date(y,m,d,hh,mm,ss).getTime() / 1000);
+  const date = Math.round(new Date(y, m, d, hh, mm, ss).getTime() / 1000);
 
-  if(isNaN(date))
-  {
-  toEpochOutput.innerHTML = "Please Input Valid Local Date!";
-  }
-  
-  else
-  {
+  if (isNaN(date)) {
+    toEpochOutput.innerHTML = "Please Input Valid Local Date!";
+  } else {
     toEpochOutput.innerHTML = date;
   }
 };
-
 
 fetch("https://api.ipify.org/?format=json")
   .then((response) => response.json())
@@ -82,5 +71,5 @@ fetch("https://api.ipify.org/?format=json")
 //Events
 setInterval(function () {
   unixTimestamp.innerHTML =
-    "CURRENT UNIX TIMESTAMP IS-" + Math.round(Date.now() / 1000)+"s";
+    "CURRENT UNIX TIMESTAMP IS-" + Math.round(Date.now() / 1000) + "s";
 }, 1000);
